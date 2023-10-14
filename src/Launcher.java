400d2c1b22108244889bd1bd83365fae4e70c0f2
Matteo Stelbrink <147656301+mstelbrink@users.noreply.github.com>
2023-10-14 14:32:04 +0200

    Added project files
    Signed-off-by: Matteo Stelbrink <147656301+mstelbrink@users.noreply.github.com>

---
+++ src/Launcher.java
@@ -0,0 +1,95 @@
+import org.jsoup.Connection;
+import org.jsoup.Connection.Method;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class Launcher implements ActionListener {
+
+    private JFrame frame;
+    private JTextField tf;
+    private JPasswordField pf;
+    private String username;
+    private char[] a;
+
+    public static void main( String[] args ) {
+
+        Launcher l = new Launcher();
+        l.createWindow();
+    }
+
+    public void createWindow() {
+        frame = new JFrame("Campus-Dual");
+        frame.setLayout(new GridLayout(3, 0));
+        frame.setSize(250, 150);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        tf = new JTextField();
+        pf = new JPasswordField();
+        JButton btn = new JButton("Login");
+        btn.addActionListener(this);
+
+        frame.add(tf);
+        frame.add(pf);
+        frame.add(btn);
+
+        frame.setVisible(true);
+    }
+
+    public void login() throws IOException {
+
+        Connection.Response loginForm = Jsoup.connect("https://erp.campus-dual.de/sap/bc/webdynpro/sap/" +
+                        "zba_initss?sap-client=100&sap-language=de&uri=https://selfservice.campus-dual.de/index/login")
+                .method(Connection.Method.GET)
+                .execute();
+
+        Document loginDoc = loginForm.parse();
+
+        String xsrf_token = loginDoc.select("#SL__FORM > input:nth-child(9)").first().attr("value");
+
+        Connection.Response loginResponse = Jsoup.connect("https://erp.campus-dual.de/sap/bc/webdynpro/sap/" +
+                        "zba_initss?sap-client=100&sap-language=de&uri=https://selfservice.campus-dual.de/index/login")
+                .userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0")
+                .cookies(loginForm.cookies())
+                .data("sap-login-XSRF", xsrf_token)
+                .data("sap-user", username)
+                .data("sap-password", new String(a))
+                .method(Method.POST)
+                .execute();
+
+        Document doc = Jsoup.connect("https://selfservice.campus-dual.de/acwork/index")
+                    .cookies(loginResponse.cookies())
+                    .get();
+
+        Arrays.fill(a, (char) 0);
+
+        for (int i = 1; i < 100; i++) {
+            if (doc.select("tr#node-" + i).text().isEmpty()) break;
+            String tr = doc.select("tr#node-" + i).text();
+            System.out.println(tr);
+        }
+    }
+
+    @Override
+    public void actionPerformed( ActionEvent e ) {
+        username = tf.getText();
+        if ( e.getActionCommand().equals("Login")) {
+            a = pf.getPassword();
+        }
+        try {
+            login();
+            frame.setVisible(false);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
