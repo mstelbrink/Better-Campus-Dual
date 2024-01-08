981e99cfbd05f8b834296c2409a22b02954f6f7c
Matteo Stelbrink <147656301+mstelbrink@users.noreply.github.com>
2024-01-08 20:51:28 +0100

    Fixed issue where grade would be char

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -98,6 +98,9 @@ public class Launcher implements ActionListener {
         for (int i = 1; i < 100; i++) {
             String module = doc.select("#node-" + i + " > td:nth-of-type(1)").text();
             String grade = doc.select("#node-" + i + " > td:nth-of-type(2)").text();
+            if (grade.equals("N")) {
+                break;
+            }
             String credit = doc.select("#node-" + i + " > td:nth-of-type(4)").text();
             String period = doc.select("#node-" + i + " > td:nth-of-type(8)").text();
             if (module.isEmpty() || grade.isEmpty()) {
