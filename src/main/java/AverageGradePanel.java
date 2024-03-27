319d7d0c675ecb5151ba0ff38563cf45520fb24c
Matteo Stelbrink <147656301+mstelbrink@users.noreply.github.com>
2024-03-27 19:28:37 +0100

    Minor adjustments

--- src/main/java/AverageGradePanel.java
+++ src/main/java/AverageGradePanel.java
@@ -7,13 +7,13 @@ public class AverageGradePanel extends JPanel {
                              double thirdSemesterAverage, double fourthSemesterAverage, double fifthSemesterAverage,
                              double sixthSemesterAverage) {
         setLayout(new GridLayout(7, 0));
-        add(new Label("Average Grade: " + averageGrade));
-        add(new Label("First Semester: " + firstSemesterAverage));
-        add(new Label("Second Semester: " + secondSemesterAverage));
-        add(new Label("Third Semester: " + thirdSemesterAverage));
-        add(new Label("Fourth Semester: " + fourthSemesterAverage));
-        add(new Label("Fifth Semester: " + fifthSemesterAverage));
-        add(new Label("Sixth Semester: " + sixthSemesterAverage));
+        add(new Label("Average grade: " + averageGrade));
+        add(new Label("1st semester: " + firstSemesterAverage));
+        add(new Label("2nd semester: " + secondSemesterAverage));
+        add(new Label("3rd semester: " + thirdSemesterAverage));
+        add(new Label("4th semester: " + fourthSemesterAverage));
+        add(new Label("5th semester: " + fifthSemesterAverage));
+        add(new Label("6th semester: " + sixthSemesterAverage));
         setVisible(true);
     }
 }
